ac4021582b8fd669f05abd6602842b1e01d64e89
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-04-12 16:50:52 +0200

    load data of worksheets on startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SpreadsheetService } from '@shared/services/spreadsheet.service';
-import { switchMap, tap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { SpreadsheetDataService } from '@shared/services/spreadsheet-data.service';
+import { Worksheet } from '@shared/interfaces/worksheet';
+import { Spreadsheet } from '@shared/interfaces/spreadsheet';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,17 @@ import { SpreadsheetDataService } from '@shared/services/spreadsheet-data.servic
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  spreadsheet: Spreadsheet;
+
+  allowedConfigs = {
+    type: ['Valuables', 'Breedables'],
+    subType: ['RNGs', 'Legendaries', 'Shinies', 'Competitives', 'Events'],
+    ball: ["Dream", "Safari", "Sport", "Beast", "Fast", "Moon", "Heavy", "Love", "Lure", "Level",
+      "Friend", "Poké", "Great", "Ultra", "Premier", "Dive", "Luxury", "Nest", "Net", "Repeat", "Timer", "Quick",
+      "Dusk", "Heal"],
+    includeShinies: ['true', 'false']
+  };
 
 
   constructor(private spreadsheetService: SpreadsheetService, private spreadsheetDataService: SpreadsheetDataService) {
@@ -20,9 +33,50 @@ export class AppComponent implements OnInit {
 
   loadStartupData() {
     this.spreadsheetService.getSpreadsheet('17KmYOvREddg0SpBGbATH7I15C4JsDfzl-07pnABTueE').pipe(
-      tap(spreadsheet => this.spreadsheetDataService.updateSpreadsheetInformation(spreadsheet)),
-      switchMap(q => this.spreadsheetService.getWorksheets(q.id, q.worksheets.map(worksheet => worksheet.id)))
-    ).subscribe(console.log);
+      tap(spreadsheet => {
+        this.spreadsheetDataService.updateSpreadsheetInformation(spreadsheet);
+        this.spreadsheet = spreadsheet;
+      }),
+      switchMap(q => this.spreadsheetService.getWorksheets(q.id, q.worksheets.map(worksheet => worksheet.id))),
+      map(worksheets => {
+        let selectedWorksheet: Worksheet;
+        for (const worksheet of worksheets) {
+          selectedWorksheet = this.spreadsheet.worksheets.filter(fWorksheet => fWorksheet.title === worksheet.feed.title['$t'])?.[0];
+
+          selectedWorksheet.config = this.getWorksheetConfig(worksheet);
+          if (selectedWorksheet) {
+            selectedWorksheet.data = worksheet.feed.entry.slice(3); // first 3 rows contain meta data
+          }
+          console.log(worksheet.feed.title['$t']);
+        }
+
+        this.spreadsheetDataService.updateSpreadsheetInformation(this.spreadsheet);
+      })
+    ).subscribe({
+      error: error => console.log('error: ', error)
+    })
+  }
+
+
+  getWorksheetConfig(worksheet) {
+    let config = {};
+    let configIndex = 0;
+    let tempConfig;
+
+    while (worksheet.feed.entry[configIndex]?.['gsx$config']?.['$t'] && configIndex < 3) {
+      tempConfig = worksheet.feed.entry[configIndex]['gsx$config']['$t'].split(':');
+      if (tempConfig.length === 2) {
+        const [key, value] = tempConfig;
+        if (key in this.allowedConfigs) {
+          if (this.allowedConfigs[key].includes(value)) {
+            config[key] = value;
+          }
+        }
+      }
+      configIndex++;
+    }
+
+    return config;
   }
 
 
