319b055315a576730f42447d2af9d92fd9417cd7
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-04-12 13:46:37 +0200

    generated header component for layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule
   ]
