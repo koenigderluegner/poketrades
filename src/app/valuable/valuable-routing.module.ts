cd2b23a38fb49b7be58633792f5f5e8191f5ac66
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-04-12 15:21:48 +0200

    generated valuable index component/route

--- src/app/valuable/valuable-routing.module.ts
+++ src/app/valuable/valuable-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/valuable/valuable.module.ts
+++ src/app/valuable/valuable.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IndexComponent } from './components/index/index.component';
+import { ValuableRoutingModule } from './valuable-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IndexComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ValuableRoutingModule
   ]
 })
 export class ValuableModule { }
