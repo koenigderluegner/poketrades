e6bb7aef4b73e9b0f39b449bf3a36d4d35b6f2b6
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-04-12 23:42:08 +0200

    generated grid item component

--- src/app/grid/grid.module.ts
+++ src/app/grid/grid.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MinimalComponent } from './minimal/minimal.component';
 import { GridComponent } from './grid.component';
+import { GridItemComponent } from './grid-item/grid-item.component';
 
 
 
 @NgModule({
-  declarations: [MinimalComponent, GridComponent],
+  declarations: [MinimalComponent, GridComponent, GridItemComponent],
   imports: [
     CommonModule
   ]
