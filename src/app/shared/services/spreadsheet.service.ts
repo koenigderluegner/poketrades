cf93308578ce79d93680fdacfade8826a4d50a8b
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-04-12 12:21:33 +0200

    added getSpreadsheet to spreadsheet.service.ts

--- src/app/shared/services/spreadsheet.service.ts
+++ src/app/shared/services/spreadsheet.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Spreadsheet } from '@shared/interfaces/spreadsheet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpreadsheetService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getSpreadsheet(spreadsheetId: string): Observable<Spreadsheet> {
+    return this.httpClient.get(`https://spreadsheets.google.com/feeds/worksheets/${spreadsheetId}/public/basic?alt=json`).pipe(
+      map((response: any) => {
+        return {
+          id: spreadsheetId,
+          title: response.feed.title['$t'],
+          date: new Date(Date.parse(response.feed.updated['$t'])),
+          worksheets: response.feed.entry
+        }
+      })
+    )
+  }
 }
