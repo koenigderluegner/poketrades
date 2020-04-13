56c82ff9b1a3ff3cd9e6ba0b95054d5d9b296d01
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-04-13 14:43:41 +0200

    clean up subscriptions in ball view

--- src/app/breedable/components/ball/ball.component.ts
+++ src/app/breedable/components/ball/ball.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest } from 'rxjs';
+import { combineLatest, Subscription } from 'rxjs';
 import { SpreadsheetDataService } from '@shared/services/spreadsheet-data.service';
 import { Worksheet } from '@shared/interfaces/worksheet';
 
@@ -9,10 +9,12 @@ import { Worksheet } from '@shared/interfaces/worksheet';
   templateUrl: './ball.component.html',
   styleUrls: ['./ball.component.scss']
 })
-export class BallComponent implements OnInit {
+export class BallComponent implements OnInit, OnDestroy {
 
   ball: string;
   worksheet: Worksheet;
+
+  subscriptions: Subscription[] = [];
 
   constructor(
     private spreadsheetDataService: SpreadsheetDataService,
@@ -21,18 +23,26 @@ export class BallComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    combineLatest([
-      this.route.paramMap,
-      this.spreadsheetDataService.getSpreadsheetInformation()
-    ]).subscribe({
-      next: ([params, spreadsheetData]) => {
-        console.log(spreadsheetData.worksheets[0].config);
-        this.ball =  params.get('ball');
-        this.worksheet = spreadsheetData.worksheets.filter(
-          worksheet => worksheet.config?.ball?.toLowerCase() === this.ball
-        )?.[0]
-      }
-    })
 
+    this.subscriptions.push(combineLatest([
+        this.route.paramMap,
+        this.spreadsheetDataService.getSpreadsheetInformation()
+      ]).subscribe({
+        next: ([params, spreadsheetData]) => {
+          console.log(spreadsheetData.worksheets[0].config);
+          this.ball = params.get('ball');
+          this.worksheet = spreadsheetData.worksheets.filter(
+            worksheet => worksheet.config?.ball?.toLowerCase() === this.ball
+          )?.[0]
+        }
+      })
+    );
+
+  }
+
+  ngOnDestroy(): void {
+    for (let subscribtion of this.subscriptions) {
+      subscribtion.unsubscribe();
+    }
   }
 }
