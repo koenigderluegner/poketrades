db1f3e072d18d1e5e8251a9a5db081d35219dfae
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-04-18 22:08:16 +0200

    generated spreadsheet changer index.component

--- src/app/spreadsheet-changer/spreadsheet-changer.module.ts
+++ src/app/spreadsheet-changer/spreadsheet-changer.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IndexComponent } from './index/index.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IndexComponent],
   imports: [
     CommonModule
   ]
