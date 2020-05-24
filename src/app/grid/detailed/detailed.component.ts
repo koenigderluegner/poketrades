5defe4e54db440fe4f0011787daae7af9a4288dc
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-05-24 20:04:44 +0200

    base grid style for normal grid

--- src/app/grid/detailed/detailed.component.ts
+++ src/app/grid/detailed/detailed.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Pokemon } from '@shared/interfaces/pokemon';
 
 @Component({
   selector: 'app-detailed',
   templateUrl: './detailed.component.html',
-  styleUrls: ['./detailed.component.scss']
+  styleUrls: ['./detailed.component.scss'],
+  host: {class: 'grid-item'},
+  encapsulation: ViewEncapsulation.None
 })
 export class DetailedComponent implements OnInit {
+
+  @Input() pokemon: Pokemon;
 
   constructor() { }
 
--- src/app/grid/normal/normal.component.ts
+++ src/app/grid/normal/normal.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Pokemon } from '@shared/interfaces/pokemon';
 
 @Component({
   selector: 'app-normal',
   templateUrl: './normal.component.html',
-  styleUrls: ['./normal.component.scss']
+  styleUrls: ['./normal.component.scss'],
+  host: {class: 'grid-item'},
+  encapsulation: ViewEncapsulation.None
 })
 export class NormalComponent implements OnInit {
 
-  constructor() { }
+  _pokemon: Pokemon;
+
+  @Input()
+  public get pokemon(): Pokemon {
+    return this._pokemon;
+  }
+
+  public set pokemon(pokemon: Pokemon) {
+    this._pokemon = pokemon;
+    // when the pokemon is set check it and set class
+    this.isInactive = !this._pokemon.isOwned;
+  }
+
+
+  @HostBinding('class.inactive') isInactive: boolean = !this.pokemon?.isOwned;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
 }
