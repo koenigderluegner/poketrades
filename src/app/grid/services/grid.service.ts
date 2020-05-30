bb474c965445424c01815c9944dfc5df05f45465
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-05-30 12:03:31 +0200

    added grid service

---
+++ src/app/grid/services/grid.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { GridAppearanceType } from '../grid-appearance.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GridService {
+
+  gridAppearance: BehaviorSubject<GridAppearanceType>;
+
+  constructor() {
+    this.gridAppearance = new BehaviorSubject<GridAppearanceType>('minimal');
+  }
+
+  getGridAppearance$(): Observable<GridAppearanceType> {
+    return this.gridAppearance.asObservable();
+  }
+
+  updateGridAppearance$(gridAppearanceType: GridAppearanceType): void {
+    this.gridAppearance.next(gridAppearanceType);
+  }
+
+}
