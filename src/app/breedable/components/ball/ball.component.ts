5ddc275cc91e2b9cc46de34d6b6074aea7f76ab6
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-05-30 12:39:13 +0200

    ball component now changes grid appearance based on grid service

--- src/app/breedable/components/ball/ball.component.ts
+++ src/app/breedable/components/ball/ball.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Worksheet } from '@spreadsheet/models/worksheet';
 import { SlugifyPipe } from '@shared/pipes/slugify.pipe';
 import { switchMap, tap } from 'rxjs/operators';
 import { SpreadsheetFacade } from '@spreadsheet/spreadsheet.facade';
+import { GridService } from '../../../grid/services/grid.service';
+import { GridAppearanceType } from '../../../grid/grid-appearance.type';
 
 @Component({
   selector: 'app-ball',
@@ -17,15 +19,19 @@ export class BallComponent implements OnInit, OnDestroy {
   worksheet: Worksheet;
 
   subscriptions: Subscription[] = [];
+  gridAppearance$: Observable<GridAppearanceType>;
 
   constructor(
     private spreadsheetFacade: SpreadsheetFacade,
     private route: ActivatedRoute,
-    private slugifyPipe: SlugifyPipe
+    private slugifyPipe: SlugifyPipe,
+    private gridService: GridService
   ) {
   }
 
   ngOnInit(): void {
+
+    this.gridAppearance$ = this.gridService.getGridAppearance$();
 
     this.route.paramMap.pipe(
       tap(params => this.worksheetTitle = params.get('worksheetTitle')),
@@ -47,7 +53,7 @@ export class BallComponent implements OnInit, OnDestroy {
     );
   }
 
-  trackBy(index, pokemon){
+  trackBy(index, pokemon) {
     return index + pokemon.dex;
   }
 
