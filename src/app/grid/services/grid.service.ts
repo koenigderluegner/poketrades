3b885e9cc527a281da5c257078f65172759ade80
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-05-31 16:12:10 +0200

    added hidden items state to grid service

--- src/app/grid/services/grid.service.ts
+++ src/app/grid/services/grid.service.ts
@@ -8,9 +8,11 @@ import { GridAppearanceType } from '../grid-appearance.type';
 export class GridService {
 
   gridAppearance: BehaviorSubject<GridAppearanceType>;
+  hideInactiveItems: BehaviorSubject<boolean>;
 
   constructor() {
     this.gridAppearance = new BehaviorSubject<GridAppearanceType>('minimal');
+    this.hideInactiveItems = new BehaviorSubject<boolean>(false);
   }
 
   getGridAppearance$(): Observable<GridAppearanceType> {
@@ -19,6 +21,14 @@ export class GridService {
 
   updateGridAppearance(gridAppearanceType: GridAppearanceType): void {
     this.gridAppearance.next(gridAppearanceType);
+  }
+
+  getHideItems$(): Observable<boolean> {
+    return this.hideInactiveItems.asObservable();
+  }
+
+  updateHideItems$(hideItems: boolean): void {
+    return this.hideInactiveItems.next(hideItems);
   }
 
 }
