8598a20be28267657dea12e29f9c404b13c5786c
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-06-03 18:58:31 +0200

    first try of self generated pokesprite, changed pokemon icon from ng-content to attribute binding

--- src/app/icon/pokemon/pokemon.component.ts
+++ src/app/icon/pokemon/pokemon.component.ts
@@ -1,53 +1,17 @@
-import {
-  AfterContentChecked,
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  HostBinding,
-  OnInit, ViewEncapsulation
-} from '@angular/core';
+import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon',
-  template: '<div (cdkObserveContent)="projectContentChanged($event)"><ng-content></ng-content></div>',
-  styleUrls: ['./pokemon.component.scss'],
+  template: '',
+  styleUrls: ['./pokemon.component.scss', '../pokesprite/pokesprite.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class PokemonComponent implements OnInit, AfterContentChecked, AfterViewInit {
+export class PokemonComponent {
 
   @HostBinding('class.pkspr') true;
+  @Input() @HostBinding('class') slug: string;
 
-  constructor(private elementRef: ElementRef, private cd: ChangeDetectorRef) {
+  constructor() {
   }
 
-  ngOnInit(): void {
-
-  }
-
-  ngAfterContentChecked(): void {
-    this.cd.markForCheck();
-  }
-
-
-  ngAfterViewInit() {
-
-    const slug = this.elementRef.nativeElement.textContent;
-    this.setImage(slug);
-
-  }
-
-  projectContentChanged($event) {
-    const slug = $event[0].target.data;
-    this.setImage(slug);
-
-  }
-
-  private setImage(slug) {
-    const img = new Image();
-    img.src = `https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/${slug}.png`;
-
-    this.elementRef.nativeElement.removeChild(this.elementRef.nativeElement.childNodes[0]);
-    this.elementRef.nativeElement.appendChild(img);
-  }
 }
