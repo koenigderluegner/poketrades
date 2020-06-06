9b18cb8e8bd349616396a352760eab3d9e854742
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-06-06 17:38:04 +0200

    added getUser to userservice

--- src/app/database/services/user.service.ts
+++ src/app/database/services/user.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+
+  private db;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  loadDatabase(): Observable<any[]> {
+    if (this.db) {
+      return of(this.db)
+    } else {
+      return this.httpClient.get<any[]>('assets/database/users.json').pipe(
+        tap(database => {
+          this.db = database;
+        })
+      )
+    }
+
+
+  }
+
+  findUser(name: string) {
+    return this.loadDatabase().pipe(
+      switchMap(database => {
+        return database[name] ? of(database[name]) : throwError('No user found with name: ' + name);
+      })
+    )
+  }
 }
