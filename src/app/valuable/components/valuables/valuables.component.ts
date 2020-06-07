526f079e26c3ad652c04de133d31003f93ae1a5d
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-06-07 15:40:32 +0200

    generated valuables component to list valuables

---
+++ src/app/valuable/components/valuables/valuables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-valuables',
+  templateUrl: './valuables.component.html',
+  styleUrls: ['./valuables.component.scss']
+})
+export class ValuablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/valuable/valuable.module.ts
+++ src/app/valuable/valuable.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './components/index/index.component';
 import { ValuableRoutingModule } from './valuable-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '@shared/shared.module';
+import { GridModule } from '../grid/grid.module';
+import { ValuablesComponent } from './components/valuables/valuables.component';
 
 
 
 @NgModule({
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, ValuablesComponent],
   imports: [
     CommonModule,
     ValuableRoutingModule
