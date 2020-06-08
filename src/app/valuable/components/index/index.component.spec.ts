5e1b09ebdce9a9aaf879a0999ff225f8f8c60522
Niersmann, Alexander <alexander.niersmann@mt-ag.com>
2020-06-08 22:04:19 +0200

    fixed tests

--- src/app/valuable/components/index/index.component.spec.ts
+++ src/app/valuable/components/index/index.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IndexComponent } from './index.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('IndexComponent', () => {
   let component: IndexComponent;
@@ -8,6 +9,7 @@ describe('IndexComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ IndexComponent ]
     })
     .compileComponents();
