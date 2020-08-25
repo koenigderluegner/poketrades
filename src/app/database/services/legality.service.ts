1974b03d2d276e682000a8644e7123fe3bef840e
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-08-25 19:50:08 +0200

    added legality service fetch

--- src/app/database/services/legality.service.ts
+++ src/app/database/services/legality.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LegalityService {
 
-  constructor() { }
+  private db;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  loadDatabase(): Observable<any[]> {
+    if (this.db) {
+      return of(this.db);
+    } else {
+      return this.httpClient.get<any[]>('assets/database/breedables-legality.json').pipe(
+        tap(database => {
+          this.db = database;
+        })
+      );
+    }
+  }
+
 }
