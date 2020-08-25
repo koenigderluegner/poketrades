c193e09467981bc431295eb93219d334b1ae084c
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-08-25 17:21:58 +0200

    generated item icon component

--- src/app/icon/icon.module.ts
+++ src/app/icon/icon.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonComponent } from './pokemon/pokemon.component';
+import { ItemComponent } from './item/item.component';
 
 
 
 @NgModule({
-  declarations: [PokemonComponent],
+  declarations: [PokemonComponent, ItemComponent],
   exports: [
     PokemonComponent
   ],
