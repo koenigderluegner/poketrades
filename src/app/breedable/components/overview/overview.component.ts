fbe3200f39e85fab01317d32bd63ca451b5c9992
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-08-25 21:05:35 +0200

    added overview table

--- src/app/breedable/components/overview/overview.component.ts
+++ src/app/breedable/components/overview/overview.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { SpreadsheetFacade } from '@spreadsheet/spreadsheet.facade';
+import { combineLatest, Observable, of } from 'rxjs';
+import { DatabaseFacadeService } from '../../../database/database-facade.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss']
+  styleUrls: ['./overview.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class OverviewComponent implements OnInit {
 
-  constructor() { }
+  data$: Observable<any>;
+
+  constructor(private facade: SpreadsheetFacade,
+              private db: DatabaseFacadeService) {
+  }
 
   ngOnInit(): void {
+    this.data$ = combineLatest([this.db.getBreedableLegality(),
+      this.facade.getCurrentSpreadsheet$()]
+    ).pipe(
+      switchMap(val => {
+        console.log(val[1]);
+        return of({legality: val[0], entries: val[1].overviewEntries});
+      })
+    );
   }
 
 }
