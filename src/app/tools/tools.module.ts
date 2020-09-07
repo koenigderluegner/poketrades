9d8e0ed328483d8d3ad784471b07a56e62c891ff
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-09-07 20:23:32 +0200

    generated tools module/component

---
+++ src/app/tools/tools.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolsComponent } from './tools.component';
+
+
+
+@NgModule({
+  declarations: [ToolsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ToolsModule { }
