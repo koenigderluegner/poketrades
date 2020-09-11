c23a44c673a92bca160a7fce33b23b36c9986805
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-09-11 23:28:34 +0200

    implemented filter-level-up-moves.pipe

--- src/app/tools/pipes/filter-level-up-moves.pipe.ts
+++ src/app/tools/pipes/filter-level-up-moves.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterLevelUpMovesPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any[], move: string): unknown {
+    if (!value) {
+      return [];
+    }
+    return value.filter(moveObj => {
+      return moveObj.move === move;
+    });
   }
 
 }
