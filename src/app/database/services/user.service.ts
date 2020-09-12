a5bb615ca7a0fb071e0c902c179eb6d0cb2d80f9
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-09-12 12:46:55 +0200

    added typing to user service

--- src/app/database/services/user.service.ts
+++ src/app/database/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { switchMap, tap } from 'rxjs/operators';
+import { UserDatabase } from '../models/user-database.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,16 @@ import { switchMap, tap } from 'rxjs/operators';
 export class UserService {
 
 
-  private db;
+  private db: UserDatabase | undefined;
 
   constructor(private httpClient: HttpClient) {
   }
 
-  loadDatabase(): Observable<any[]> {
+  loadDatabase(): Observable<UserDatabase> {
     if (this.db) {
       return of(this.db);
     } else {
-      return this.httpClient.get<any[]>('assets/database/users.json').pipe(
+      return this.httpClient.get<UserDatabase>('assets/database/users.json').pipe(
         tap(database => {
           this.db = database;
         })
@@ -28,7 +29,7 @@ export class UserService {
 
   }
 
-  findUser(name: string) {
+  findUser(name: string): Observable<string> {
     return this.loadDatabase().pipe(
       switchMap(database => {
         return database[name] ? of(database[name]) : throwError('No user found with name: ' + name);
