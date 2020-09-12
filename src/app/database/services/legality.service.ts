1ff717d7e53ef8b03461bad1aa7c0c22093ccb5a
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-09-12 11:59:42 +0200

    added interface to legality service, removed icon field

--- src/app/database/services/legality.service.ts
+++ src/app/database/services/legality.service.ts
@@ -2,22 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { LegalityEntry } from '../models/legality-entry.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LegalityService {
 
-  private db;
+  private db: LegalityEntry[];
 
   constructor(private httpClient: HttpClient) {
   }
 
-  loadDatabase(): Observable<any[]> {
+  loadDatabase(): Observable<LegalityEntry[]> {
     if (this.db) {
       return of(this.db);
     } else {
-      return this.httpClient.get<any[]>('assets/database/breedables-legality.json').pipe(
+      return this.httpClient.get<LegalityEntry[]>('assets/database/breedables-legality.json').pipe(
         tap(database => {
           this.db = database;
         })
@@ -25,7 +26,7 @@ export class LegalityService {
     }
   }
 
-  getList() {
+  getList(): Observable<LegalityEntry[]> {
     return this.loadDatabase();
   }
 
