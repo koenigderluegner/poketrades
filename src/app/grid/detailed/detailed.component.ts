9f8a0a600a11878e71c89340e9ae9d092e97436b
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-09-13 22:43:51 +0200

    added typing to grid detailed

--- src/app/grid/detailed/detailed.component.ts
+++ src/app/grid/detailed/detailed.component.ts
@@ -1,7 +1,9 @@
 import { Component, HostBinding, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
 import { Pokemon } from '@shared/interfaces/pokemon';
 import { DatabaseFacadeService } from '../../database/database-facade.service';
-import { forkJoin, Observable } from 'rxjs';
+import { forkJoin, Observable, of } from 'rxjs';
+import { PokemonEntry } from '../../database/models/pokemon-entry-.interface';
+import { Move } from '../../database/models/move.interface';
 
 @Component({
   selector: 'app-detailed',
@@ -11,16 +13,16 @@ import { forkJoin, Observable } from 'rxjs';
 })
 export class DetailedComponent implements OnInit, OnChanges {
 
-  @HostBinding('class.grid-item') true;
-  isEggMove$: Observable<boolean[]>;
+  @HostBinding('class.grid-item') isGridItem = true;
+  isEggMove$: Observable<boolean[]> | undefined;
 
   @HostBinding('class.inactive') get inactive() {
     return !this.pokemon?.isOwned;
   }
 
-  @Input() pokemon: Pokemon;
-  dbpokemon$: Observable<any>;
-  moves$: Observable<any[]>;
+  @Input() pokemon: Pokemon | undefined;
+  dbpokemon$: Observable<PokemonEntry> | undefined;
+  moves$: Observable<Move[]> | undefined;
 
   constructor(private databaseFacadeService: DatabaseFacadeService) {
   }
@@ -33,15 +35,17 @@ export class DetailedComponent implements OnInit, OnChanges {
     this.initPokemonAttributes();
   }
 
-  initPokemonAttributes() {
-    this.dbpokemon$ = this.databaseFacadeService.findPokemon(this.pokemon?.name);
-    this.moves$ = forkJoin(this.pokemon.moves.map(move => {
-      return this.databaseFacadeService.findMove(move);
-    }));
+  initPokemonAttributes(): void {
+    if (this.pokemon) {
+      this.dbpokemon$ = this.databaseFacadeService.findPokemon(this.pokemon?.name);
+      this.moves$ = forkJoin(this.pokemon.moves.map(move => {
+        return this.databaseFacadeService.findMove(move);
+      }));
 
-    this.isEggMove$ = forkJoin(this.pokemon.moves.map(move => {
-      return this.databaseFacadeService.isEggMove(this.pokemon.name, move);
-    }));
+      this.isEggMove$ = forkJoin(this.pokemon.moves.map(move => {
+        return this.pokemon ? this.databaseFacadeService.isEggMove(this.pokemon.name, move) : of(false);
+      }));
+    }
   }
 
 }
