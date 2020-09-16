9ea4b489d469e259d0b5668aa4970d115f3d698f
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-09-16 20:52:24 +0200

    fixed sub-navi-item typings

--- src/app/shared/components/sub-navi-item/sub-navi-item.component.ts
+++ src/app/shared/components/sub-navi-item/sub-navi-item.component.ts
@@ -1,19 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-sub-navi-item',
   templateUrl: './sub-navi-item.component.html',
-  styleUrls: ['./sub-navi-item.component.scss']
+  styleUrls: ['./sub-navi-item.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class SubNaviItemComponent implements OnInit {
+export class SubNaviItemComponent {
 
-  @Input() text: string;
-  @Input() link: any[];
-  @Input() meta: string;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input() text: string | undefined;
+  @Input() link: (string | number)[] | string | null | undefined;
+  @Input() meta: string | undefined;
 
 }
