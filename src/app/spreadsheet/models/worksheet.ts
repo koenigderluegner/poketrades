a6a47381492e9a82143700fc1c24b2c68c0f1640
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-09-18 20:38:19 +0200

    changed Worksheet.config to WorksheetConfig type

--- src/app/spreadsheet/models/worksheet.ts
+++ src/app/spreadsheet/models/worksheet.ts
@@ -1,10 +1,11 @@
 import { Pokemon } from '@shared/interfaces/pokemon';
+import { WorksheetConfig } from '@spreadsheet/models/worksheet-config.interface';
 
 export interface Worksheet {
   id: string;
   title: string;
   updated: Date;
-  config?: any;
+  config?: WorksheetConfig;
   data?: Pokemon[];
   ownedEntries?: number;
 }
