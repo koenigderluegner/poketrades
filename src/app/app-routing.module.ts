075db7b20ad4d3972a6e046f5493029ffa83eb94
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-09-26 13:26:41 +0200

    made tools route available for user routes too

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
+const toolsRoutes = {
+  path: 'tools',
+  loadChildren: () => import('./tools/tools.module').then(m => m.ToolsModule)
+};
+
+
 const userRoutes: Routes = [
   {path: 'breedables', loadChildren: () => import('./breedable/breedable.module').then(m => m.BreedableModule)},
-  {path: 'valuables', loadChildren: () => import('./valuable/valuable.module').then(m => m.ValuableModule)}
+  {path: 'valuables', loadChildren: () => import('./valuable/valuable.module').then(m => m.ValuableModule)},
 ];
 
+userRoutes.push(toolsRoutes);
 
 const routes: Routes = [
   {
     path: 'change-spreadsheet',
     loadChildren: () => import('./spreadsheet-changer/spreadsheet-changer.module').then(m => m.SpreadsheetChangerModule)
   },
-  {
-    path: 'tools',
-    loadChildren: () => import('./tools/tools.module').then(m => m.ToolsModule)
-  },
+
   {
     path: ':spreadsheetId', children: userRoutes
   },
@@ -26,6 +30,8 @@ const routes: Routes = [
 
 
 ];
+
+routes.push(toolsRoutes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
