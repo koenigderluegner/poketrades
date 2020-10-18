926aa1839224bfa2273be433d385d8bd21419ffa
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-10-18 23:08:18 +0200

    added service methods to hide controls of grid controller

--- src/app/grid/grid-controller/grid-controller.component.ts
+++ src/app/grid/grid-controller/grid-controller.component.ts
@@ -2,6 +2,7 @@ import { Component, HostBinding, OnInit, ViewEncapsulation } from '@angular/core
 import { GridAppearanceType } from '../grid-appearance.type';
 import { FormControl } from '@angular/forms';
 import { GridService } from '../services/grid.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-grid-controller',
@@ -14,10 +15,14 @@ export class GridControllerComponent implements OnInit {
   @HostBinding('class.grid-controller') setClass = true;
 
   toggleInactivesControl: FormControl;
+  hideAppearance$: Observable<boolean>;
+  hideInactives$: Observable<boolean>;
 
 
   constructor(private gridService: GridService) {
     this.toggleInactivesControl = new FormControl(this.gridService.getHideItems());
+    this.hideAppearance$ = this.gridService.getHideAppearanceControl$();
+    this.hideInactives$ = this.gridService.getHideInactiveItemsControl$();
   }
 
   ngOnInit(): void {
--- src/app/grid/services/grid.service.ts
+++ src/app/grid/services/grid.service.ts
@@ -9,10 +9,14 @@ export class GridService {
 
   gridAppearance: BehaviorSubject<GridAppearanceType>;
   hideInactiveItems: BehaviorSubject<boolean>;
+  hideAppearanceControl: BehaviorSubject<boolean>;
+  hideInactiveItemsControl: BehaviorSubject<boolean>;
 
   constructor() {
     this.gridAppearance = new BehaviorSubject<GridAppearanceType>('minimal');
     this.hideInactiveItems = new BehaviorSubject<boolean>(false);
+    this.hideAppearanceControl = new BehaviorSubject<boolean>(false);
+    this.hideInactiveItemsControl = new BehaviorSubject<boolean>(false);
   }
 
   getGridAppearance$(): Observable<GridAppearanceType> {
@@ -33,6 +37,22 @@ export class GridService {
 
   updateHideItems(hideItems: boolean): void {
     return this.hideInactiveItems.next(hideItems);
+  }
+
+  getHideInactiveItemsControl$(): Observable<boolean> {
+    return this.hideInactiveItemsControl.asObservable();
+  }
+
+  updateHideInactiveItemsControl(hideControl: boolean): void {
+    return this.hideInactiveItemsControl.next(hideControl);
+  }
+
+  getHideAppearanceControl$(): Observable<boolean> {
+    return this.hideAppearanceControl.asObservable();
+  }
+
+  updateHideAppearanceControl(hideControl: boolean): void {
+    return this.hideAppearanceControl.next(hideControl);
   }
 
 }
