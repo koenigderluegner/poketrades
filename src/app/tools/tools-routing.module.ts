4209ce3b1653baecf564a950d071888356e15717
Alexander Niersmann <alexander.niersmann@mt-ag.com>
2020-10-18 13:40:10 +0200

    added shiny dex to routing

--- src/app/tools/tools-routing.module.ts
+++ src/app/tools/tools-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ToolsComponent } from './tools.component';
 import { BreedingComponent } from './breeding/breeding.component';
+import { ShinyDexComponent } from './components/shiny-dex/shiny-dex.component';
 
 
 const routes: Routes = [
@@ -9,6 +10,9 @@ const routes: Routes = [
     path: '', component: ToolsComponent, children: [
       {
         path: 'breeding', component: BreedingComponent
+      } ,
+      {
+        path: 'shiny-dex', component: ShinyDexComponent
       }
     ]
   }
