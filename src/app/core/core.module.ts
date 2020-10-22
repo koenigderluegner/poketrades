23c91a25c11c24da58ea6a426694963ce880e7f1
Alexander <aniersmann@gmail.com>
2020-10-23 01:51:29 +0200

    added mobile pop up navigation for smaller devices

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {OverlayModule} from '@angular/cdk/overlay';
+import {SlugifyPipe} from '@shared/pipes/slugify.pipe';
 
 
 
@@ -14,7 +18,13 @@ import { SharedModule } from '@shared/shared.module';
   imports: [
     CommonModule,
     RouterModule,
-    SharedModule
+    SharedModule,
+    MatIconModule,
+    MatButtonModule,
+    OverlayModule
+  ],
+  providers: [
+    SlugifyPipe
   ]
 })
 export class CoreModule { }
