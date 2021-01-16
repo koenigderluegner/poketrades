86adede3fe7c8d1126413dbac033526b4efab691
Alexander <aniersmann@gmail.com>
2021-01-16 19:26:08 +0100

    fixed tests

--- src/app/core/components/layout/header/header.component.spec.ts
+++ src/app/core/components/layout/header/header.component.spec.ts
@@ -1,8 +1,10 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import {HeaderComponent} from './header.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {OverlayModule} from '@angular/cdk/overlay';
+import {SlugifyPipe} from '@shared/pipes/slugify.pipe';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -10,8 +12,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],
-      declarations: [HeaderComponent]
+      imports: [HttpClientTestingModule, RouterTestingModule, OverlayModule],
+      declarations: [HeaderComponent],
+      providers: [SlugifyPipe]
     })
       .compileComponents();
   }));
