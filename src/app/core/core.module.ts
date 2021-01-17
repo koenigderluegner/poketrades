5b16d38d6f066fa7a5ea6d2dc64b64f2b9cc53ad
Alexander <aniersmann@gmail.com>
2021-01-17 20:30:40 +0100

    generated footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,11 +7,12 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {OverlayModule} from '@angular/cdk/overlay';
 import {SlugifyPipe} from '@shared/pipes/slugify.pipe';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   exports: [
     HeaderComponent
   ],
