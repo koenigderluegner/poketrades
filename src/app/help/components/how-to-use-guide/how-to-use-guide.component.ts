d0223f4413c7afbc5ebdf7f8db7066a7f89647fe
Alexander <aniersmann@gmail.com>
2021-01-17 19:20:19 +0100

    generated how to use guide component

---
+++ src/app/help/components/how-to-use-guide/how-to-use-guide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-how-to-use-guide',
+  templateUrl: './how-to-use-guide.component.html',
+  styleUrls: ['./how-to-use-guide.component.scss']
+})
+export class HowToUseGuideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/help/help.module.ts
+++ src/app/help/help.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HelpRoutingModule } from './help-routing.module';
 import { HelpComponent } from './help.component';
+import { HowToUseGuideComponent } from './components/how-to-use-guide/how-to-use-guide.component';
 
 
 @NgModule({
-  declarations: [HelpComponent],
+  declarations: [HelpComponent, HowToUseGuideComponent],
   imports: [
     CommonModule,
     HelpRoutingModule
