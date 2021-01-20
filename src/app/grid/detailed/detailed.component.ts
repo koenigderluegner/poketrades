74e466feee327c2d406ac70f602e81a0c343061d
Alexander <aniersmann@gmail.com>
2021-01-20 22:40:10 +0100

    added colored natures for detailed

--- src/app/grid/detailed/detailed.component.ts
+++ src/app/grid/detailed/detailed.component.ts
@@ -1,10 +1,9 @@
-import { Component, HostBinding, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
-import { Pokemon } from '@shared/interfaces/pokemon';
-import { DatabaseFacadeService } from '../../database/database-facade.service';
-import { forkJoin, Observable, of } from 'rxjs';
-import { PokemonEntry } from '../../database/models/pokemon-entry.interface';
-import { Move } from '../../database/models/move.interface';
-import {NaturesService} from '../../database/services/natures.service';
+import {Component, HostBinding, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation} from '@angular/core';
+import {Pokemon} from '@shared/interfaces/pokemon';
+import {DatabaseFacadeService} from '../../database/database-facade.service';
+import {forkJoin, Observable, of} from 'rxjs';
+import {PokemonEntry} from '../../database/models/pokemon-entry.interface';
+import {Move} from '../../database/models/move.interface';
 
 @Component({
   selector: 'app-detailed',
@@ -16,6 +15,7 @@ export class DetailedComponent implements OnInit, OnChanges {
 
   @HostBinding('class.grid-item') isGridItem = true;
   isEggMove$: Observable<boolean[]> | undefined;
+  natureClass = '';
 
   @HostBinding('class.inactive') get inactive() {
     return !this.pokemon?.isOwned;
@@ -25,8 +25,7 @@ export class DetailedComponent implements OnInit, OnChanges {
   dbpokemon$: Observable<PokemonEntry> | undefined;
   moves$: Observable<Move[]> | undefined;
 
-  constructor(private databaseFacadeService: DatabaseFacadeService,
-              public naturesService: NaturesService) {
+  constructor(private databaseFacadeService: DatabaseFacadeService) {
   }
 
   ngOnInit(): void {
@@ -39,6 +38,10 @@ export class DetailedComponent implements OnInit, OnChanges {
 
   initPokemonAttributes(): void {
     if (this.pokemon) {
+      this.natureClass = this.pokemon.nature
+      && this.databaseFacadeService.isNature(this.pokemon.nature)
+        ? this.pokemon.nature.toLowerCase()
+        : '';
       this.dbpokemon$ = this.databaseFacadeService.findPokemon(this.pokemon?.name);
       this.moves$ = forkJoin(this.pokemon.moves.map(move => {
         return this.databaseFacadeService.findMove(move);
