1cd5c85f96ffa9f7e6ff89b2422e60f7d1b7aced
Alexander <aniersmann@gmail.com>
2021-09-03 20:00:43 +0200

    added api key placeholder to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  googleApiKey: 'YOUR_GOOGLE_API_KEY'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  googleApiKey: 'YOUR_GOOGLE_API_KEY'
 };
 
 /*
